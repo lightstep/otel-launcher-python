cc74d75c75ad54017c40e19c28a7a25e89c997d1
Diego Hurtado <ocelotl@users.noreply.github.com>
2020-06-30 17:24:43 -0600

    Fix lint

--- src/opentelemetry/lightstep/__init__.py
+++ src/opentelemetry/lightstep/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .configuration import configure_opentelemetry
+from opentelemetry.lightstep.configuration import configure_opentelemetry
--- src/opentelemetry/lightstep/tracer.py
+++ src/opentelemetry/lightstep/tracer.py
@@ -19,13 +19,12 @@ import grpc
 
 from opentelemetry import trace
 from opentelemetry.ext.otlp.trace_exporter import OTLPSpanExporter
+from opentelemetry.lightstep.version import __version__
 from opentelemetry.sdk.trace import Resource, TracerProvider
 from opentelemetry.sdk.trace.export import (
     BatchExportSpanProcessor,
     ConsoleSpanExporter,
 )
-
-from .version import __version__
 
 logger = logging.getLogger(__name__)
 
