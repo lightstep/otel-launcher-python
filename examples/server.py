ea1f3132e1139851f81ee706d3b96ec23e9cb01a
Diego Hurtado <ocelotl@users.noreply.github.com>
2020-11-04 17:21:43 -0600

    Refactor server

--- examples/server.py
+++ examples/server.py
@@ -5,20 +5,22 @@ from opentelemetry.launcher import configure_opentelemetry
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 
 
-configure_opentelemetry(
-    service_name="server-456",
-    service_version="4.5.6",
-    log_level="DEBUG",  # optional
-)
+def receive_requests():
+    configure_opentelemetry(
+        service_name="server-456",
+        service_version="4.5.6",
+        log_level="DEBUG",  # optional
+    )
 
-app = Flask(__name__)
-FlaskInstrumentor().instrument_app(app)
+    app = Flask(__name__)
+    FlaskInstrumentor().instrument_app(app)
 
+    @app.route("/hello")
+    def hello():
+        return "hello"
 
-@app.route("/hello")
-def hello():
-    return "hello"
+    app.run(host="0.0.0.0", port=8000)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    receive_requests()
