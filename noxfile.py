18b74bd17b6f3451d42584eabd7c448d4070cf79
Diego Hurtado <ocelotl@users.noreply.github.com>
2020-11-05 17:06:22 -0600

    Fix noxfile

--- noxfile.py
+++ noxfile.py
@@ -2,17 +2,24 @@ from nox import session
 
 
 @session(python=["3.5", "3.6", "3.7", "3.8"], reuse_venv=True)
-def test(session):
+def configuration(session):
     session.install(".")
     session.install("-r", "requirements-test.txt")
 
     if session.posargs is None:
-        session.run("pytest", "tests")
+        session.run("pytest", "-k", "TestConfiguration")
     else:
         session.run("pytest", *session.posargs)
 
 
-@session(python=["3.8"])
+@session(python=["3.8"], reuse_venv=True)
+def example(session):
+    session.install(".")
+    session.install("-r", "requirements-test.txt")
+    session.run("pytest", "-k", "test_example")
+
+
+@session(python=["3.8"], reuse_venv=True)
 def lint(session):
     session.install(".")
     session.install("-r", "requirements-test.txt")
@@ -22,7 +29,7 @@ def lint(session):
     session.run("pylint", "src")
 
 
-@session(python=["3.8"])
+@session(python=["3.8"], reuse_venv=True)
 def coverage(session):
     session.install(".")
     session.install("-r", "requirements-test.txt")
