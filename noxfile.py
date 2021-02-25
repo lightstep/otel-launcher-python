9ec31f7cb3c93483f7c48a070d4d268b26bbbbed
Diego Hurtado <ocelotl@users.noreply.github.com>
2021-02-25 10:14:31 -0600

    Make pip do not hang while installing
    Pip dependency resolver has had a bug fixed in the past. Apparently this
    fix makes dependency resolution more correct but it requires downloading
    of lots and lots of versions of the same package, causing pip install to
    take a very long time. This uses the old dependency resolver to be able
    to move forward. This is just a temporary measure until a better
    solution can be found.

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,8 @@ from nox import session
 
 @session(python=["3.5", "3.6", "3.7", "3.8"], reuse_venv=True)
 def test(session):
-    session.install(".")
+    # FIXME Investigate further if this is the right approach.
+    session.install("-vvv", ".", "--use-deprecated=legacy-resolver")
     session.install("-r", "requirements-test.txt")
 
     if session.posargs:
