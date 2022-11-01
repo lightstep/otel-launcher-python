ce0cb75941b8dbdc3849895e33d2e5ec349f55f3
Diego Hurtado <ocelotl@users.noreply.github.com>
2022-11-01 18:40:45 +0000

    WIP

---
+++ tests/test_metrics_example.py
@@ -0,0 +1,32 @@
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import (
+    OTLPMetricExporter,
+)
+from opentelemetry.metrics import (
+    get_meter_provider,
+    set_meter_provider,
+)
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+
+from opentelemetry.launcher import configure_opentelemetry
+
+exporter = OTLPMetricExporter(insecure=True)
+reader = PeriodicExportingMetricReader(exporter)
+provider = MeterProvider(metric_readers=[reader])
+set_meter_provider(provider)
+
+
+configure_opentelemetry(
+    metrics_enabled=True,
+    metrics_exporter_endpoint="https://ingest.staging.lightstep.com",
+    service_name="metrics_test_23354",
+    service_version="server_version",  # optional
+)
+meter = get_meter_provider().get_meter("getting-started", "0.1.2")
+
+# Counter
+counter = meter.create_counter("counter")
+
+
+def test_metrics_example(self):
+    counter.add(1)
